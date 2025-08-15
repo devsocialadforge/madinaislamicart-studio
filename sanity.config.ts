02ad02af53d6d2a9bf6301e2f9e349f6bb73a9ae
SocialAdForge <socialadforge@gmail.com>
2025-08-15 16:29:53 +0530

    some env issues

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,13 +2,14 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dlgr5z1a'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export default defineConfig({
   name: 'default',
   title: 'madina-islamic-art',
-
-  projectId: process.env.SANITY_PROJECT_ID || '',
-  dataset: process.env.SANITY_DATASET || 'production',
+  projectId,
+  dataset,
 
   plugins: [structureTool(), visionTool()],
 
